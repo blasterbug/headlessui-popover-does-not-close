759e5c3089ca19b2a95c71e23e931709c034928b
Benjamin <Benjamin.sientzoff@papperslost.se>
2021-04-15 16:13:08 +0200

    remove distraction

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,15 +2,11 @@
 
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Contacts from './Contacts';
-import Home from './Home';
 import Page from './Page';
 
 export default function Routes() {
   return (
     <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/contacts" component={Contacts} />
       <Route path="/:page" component={Page} />
     </Switch>
   );
